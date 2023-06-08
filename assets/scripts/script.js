85afc3bb0eb6e400aacf88b8ce3ac3299bd9bd12
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-06-08 13:40:33 +0000

    Add getCards function, create an array of objects, clone the array and join the two arrays together

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -38,6 +38,59 @@ function validateInput() {
 }
 
 /**
+ * Create an array of objects to add images.  Clone the array and then join them together to create pairs of cards
+ */
+function getCards() {
+
+	let originalCards = [
+		{
+			front: '../images/angelfish.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/clownfish.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/crab.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/jellyfish.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/octopus.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/seahorse.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/shark.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/starfish.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/turtle.png',
+			back: './images/back.png',
+		},
+		{
+			front: '../images/whale.png',
+			back: './images/back.png',
+		},
+	];
+
+	let clonedCards = originalCards.slice(0);
+	let cards = originalCards.concat(clonedCards);
+
+}
+
+/**
  * Hide Welcome Panel when start button is pressed and display grid
  * */
 function startGame() {	
