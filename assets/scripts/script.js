54ac779eca56b9899854dd18749f388017707ea0
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-06-17 18:03:14 +0000

    Remove unused variable

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -14,7 +14,6 @@ let counter = 0;
 let cardClicks = 0;
 let data;
 let pickedCards = [];
-let currentPlayer = getCurrentPlayer();
 
 // Wait for the DOM to finish loading before running the game
 // Get the button element and add an event listener to it, start the game if the validateInput function is true
