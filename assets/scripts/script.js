d71fa2fe26496a416ede0c561acaeb9f767f330a
Michelle Britton <michelle@daisy-webdesign.co.uk>
2023-06-23 13:22:07 +0000

    Add Credits and Acknowledgments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,8 +16,11 @@ let data;
 let pickedCards = [];
 let isPlaying = true;
 
-// Wait for the DOM to finish loading before running the game
-// Get the button element and add an event listener to it, start the game if the validateInput function is true
+/**
+ * Wait for the DOM to finish loading before running the game
+ * Get the button element and add an event listener to it, start the game if the validateInput function is true
+ * Credit: Love Maths Walkthrough Project
+ */
 document.addEventListener("DOMContentLoaded", function() {
     let button = document.getElementById('btn');
 
